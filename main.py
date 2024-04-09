cc3b94364d8a5f5c7f6797e842fcbc0f20000a21
qirangalaxy <166411227+qirangalaxy@users.noreply.github.com>
2024-04-08 22:42:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,6 @@ def send_email(sender_email, recipient_email, subject):
 def main():
     # The website address only used for illustration
     if check_availability("https://www.drewloholdings.com/apartments-for-rent/rosecliffe-gardens-ii"):
-        send_email("qiran@gmail.com","qiran@gmail.com","Available Studio $13xx at LondonApartment")
+        send_email("qiran@gmail.com","qiran@gmail.com","Available 1b $14xx at Rosecliffe Gardens II")
 
 main()
